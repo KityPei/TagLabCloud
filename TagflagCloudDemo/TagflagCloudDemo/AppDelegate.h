fc1468672fccf4de5d0fc789b86b9358c3aa7959
KityPei <Kity_Pei@163.com>
2016-05-24 10:09:29 +0800

    测试分支

---
+++ TagflagCloudDemo/TagflagCloudDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TagflagCloudDemo
+//
+//  Created by Kity_Pei on 16/5/24.
+//  Copyright © 2016年 Kity_Pei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TagflagCloudDemo/TagflagCloudDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TagflagCloudDemo
+//
+//  Created by Kity_Pei on 16/5/24.
+//  Copyright © 2016年 Kity_Pei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
